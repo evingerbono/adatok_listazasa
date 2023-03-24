dacd9262b5f721f4e3e642798b18517cd1985c88
evingerbono <bono0823@gmail.com>
2023-03-24 13:48:19 +0100

    tablazat_létrehozás_félkész

--- main.js
+++ main.js
@@ -8,22 +8,39 @@ $(function () {
     rendezes(ADATLISTA, "fajta");
 
     console.log(ADATLISTA);
-    console.log(szures(ADATLISTA,"fajta","keverék"));
+    console.log(szures(ADATLISTA, "fajta", "keverék"));
 
-    const NEVINPUTELEM=$("#nev");
-    const FAJTAINPUTELEM=$("#fajta");
-    const TABLAZATELEM=$("#table")
+    const NEVINPUTELEM = $("#nev");
+    const FAJTAINPUTELEM = $("#fajta");
 
-    NEVINPUTELEM.on("change", function() {
-        let nevErtek=NEVINPUTELEM.val();
-        let szurtlista=szures(ADATLISTA,"nev",nevErtek);
+    NEVINPUTELEM.on("change", function () {
+        let nevErtek = NEVINPUTELEM.val();
+        let szurtlista = szures(ADATLISTA, "nev", nevErtek);
         console.log(szurtlista);
     });
 
-    FAJTAINPUTELEM.on("change", function(){
-        let fajtaErtek=FAJTAINPUTELEM.val();
-        let szurtlista=szures(ADATLISTA,"fajta",fajtaErtek);
+    FAJTAINPUTELEM.on("change", function () {
+        let fajtaErtek = FAJTAINPUTELEM.val();
+        let szurtlista = szures(ADATLISTA, "fajta", fajtaErtek);
         console.log(szurtlista);
     });
+
+    const TABLAZATELEM = $("article");
+    let tartalom = megjelenit(ADATLISTA);
+    TABLAZATELEM.append(tartalom);
 })
+
+function megjelenit(lista) {
+    let txt = "";
+    txt += "<table>";
+    for (let index = 0; index < lista.length; index++) {
+        txt+="<tr>"
+        for (let kulcs in lista) {
+            txt += "<td>" + lista[index][kulcs] + "</td>";
+        }
+        txt+="</tr>"
+    }
+    txt +="</table>";
+    return txt;
+}
 
