9b19e5fee4029faaef57096b2a74a6e744564ca3
evingerbono <bono0823@gmail.com>
2023-03-28 20:34:14 +0200

    táblázat_rendezése_kattintásal_és_beírásal

--- main.js
+++ main.js
@@ -10,37 +10,72 @@ $(function () {
     console.log(ADATLISTA);
     console.log(szures(ADATLISTA, "fajta", "keverék"));
 
+    const TABLAZATELEM = $("article");
     const NEVINPUTELEM = $("#nev");
+    const KORINPUTELEM = $("#kor");
     const FAJTAINPUTELEM = $("#fajta");
 
-    NEVINPUTELEM.on("change", function () {
+
+    let tartalom = megjelenit(ADATLISTA);
+    TABLAZATELEM.append(tartalom);
+
+
+    $("th").on("click", function () {
+        let index = $(this).index();
+        let kulcs = Object.keys(ADATLISTA[0])[index];
+
+        rendezes(ADATLISTA, kulcs);
+
+        let tablaElem = $("table");
+        let ujTartalom = megjelenit(ADATLISTA);
+        tablaElem.replaceWith(ujTartalom);
+    });
+
+    NEVINPUTELEM.on("input", function () {
         let nevErtek = NEVINPUTELEM.val();
         let szurtlista = szures(ADATLISTA, "nev", nevErtek);
         console.log(szurtlista);
+        let tartalom = megjelenit(szurtlista);
+        $("table").replaceWith(tartalom);
     });
 
-    FAJTAINPUTELEM.on("change", function () {
+    KORINPUTELEM.on("input", function () {
+        let korErtek = KORINPUTELEM.val();
+        let szurtlista = szures(ADATLISTA, "kor", korErtek);
+        console.log(szurtlista);
+        let tartalom = megjelenit(szurtlista);
+        $("table").replaceWith(tartalom);
+    });
+
+    FAJTAINPUTELEM.on("input", function () {
         let fajtaErtek = FAJTAINPUTELEM.val();
         let szurtlista = szures(ADATLISTA, "fajta", fajtaErtek);
         console.log(szurtlista);
+        let tartalom = megjelenit(szurtlista);
+        $("table").replaceWith(tartalom);
     });
 
-    const TABLAZATELEM = $("article");
-    let tartalom = megjelenit(ADATLISTA);
-    TABLAZATELEM.append(tartalom);
+
 })
 
 function megjelenit(lista) {
     let txt = "";
     txt += "<table>";
+    txt += "<tr>";
+    for (let kulcs in lista[0]) {
+        txt += "<th>" + kulcs + "</th>";
+    }
+    txt += "<th></th>";
+    txt += "</tr>";
     for (let index = 0; index < lista.length; index++) {
-        txt+="<tr>"
-        for (let kulcs in lista) {
+        txt += "<tr>";
+        for (let kulcs in lista[index]) {
             txt += "<td>" + lista[index][kulcs] + "</td>";
         }
-        txt+="</tr>"
+        txt += "<td>X</td>";
+        txt += "</tr>";
     }
-    txt +="</table>";
+    txt += "</table>";
     return txt;
 }
 
