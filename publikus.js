00e6d57a5e2e124b3b1747d2ffaef7e710e4b07e
evingerbono <bono0823@gmail.com>
2023-04-14 13:44:18 +0200

    Modal_megjeleit_alap

--- publikus.js
+++ publikus.js
@@ -9,7 +9,16 @@ $(function () {
         let index = (event.target.id);
         console.log(index);
         //Modal//
-        
+        {/* <div class="modal" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body">
+                            megjelenit(ADATLISTA);
+                    </div>
+                </div>
+            </div>
+        </div> */}
+
     });
 
 })
@@ -17,12 +26,12 @@ $(function () {
 function megjelenit(lista) {
     let txt = "";
     for (let index = 0; index < lista.length; index++) {
-        txt +=  "<div class='card col-sm-4' >";
+        txt += "<div class='card col-sm-4' >";
         txt += "<div class='card-body'>";
         for (let kulcs in lista[index]) {
             txt += "<h3 class='card-title'>" + lista[index][kulcs] + "</h3>";
         }
-        txt+="<a href=# class='nagyit btn btn-primary' id='" + index + "'>Mutat</a>";
+        txt += "<a href=# class='nagyit btn btn-primary' data-bs-toggle='modal' data-bs-target='#myModal' id='" + index + "'>Mutat</a>";
         txt += "</div>";
         txt += "</div>";
     }
